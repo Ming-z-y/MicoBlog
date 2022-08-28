915809eebd6cccbe3189d16edc915188ab44c8e7
‘ruijiaFor’ <3522443660@qq.com>
2022-08-28 20:10:34 +0800

    静态

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import 'antd/dist/antd.css';
+import './index.css'
 export default function Article() {
     return (
-        <div>Article页面</div>
+        <div id='wrap'>
+            <div id="title"><Link to='/column'>专栏标题</Link></div>
+            <div id="content">
+                <h1>《拒绝摆烂人生》</h1>
+                <div className="msgbox">
+                    <img src="../../assets/img.JPG" alt="" />
+                    <div className="name">phy</div>
+                    <div className="time">07-14</div>
+                </div>
+               <div className="wordbox">
+                 <p>
+                    <span> 首先学生自己要明白，上大学的意义，端正学习态度。其次，这个时候辅导员的重要性就更加明显了。</span>
+                    <span>进入大学，肯定会鼓励学生积极参加社团活动丰富大学生活，但是也要告诉学生不要被五光十色的大学生活迷乱了眼。</span>
+                </p>
+               </div>
+            </div>
+        </div>
     )
 }
--- src/pages/Column/index.jsx
+++ src/pages/Column/index.jsx
@@ -1,7 +1,63 @@
 import React from 'react'
-
+import 'antd/dist/antd.css';
+import { Descriptions,List } from 'antd';
+import { Link } from 'react-router-dom';
+import './index.css'
 export default function Column() {
+    const data = [
+        {
+          title: 'Ant Design Title 1',
+        },
+        {
+          title: 'Ant Design Title 2',
+        },
+        {
+          title: 'Ant Design Title 3',
+        },
+        {
+          title: 'Ant Design Title 4',
+        },
+      ];    
     return (
-        <div>Column页面</div>
+        <div id='wrap'>
+            <div id="top">
+                <div id="top-left">
+                    <div id="img-box">
+                    </div>
+                </div>
+                <div id="top-right">
+                    <Descriptions title="大学生就该这样" style={{ fontSize: '20px' }}>
+                        <Descriptions.Item>
+                            简介：Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.
+                        </Descriptions.Item>
+                    </Descriptions>
+                    <div id="msgbox">
+                      <div id="imgbox">
+                      <img src="../../assets/react.svg" alt="" />                  
+                      <div id="name">ply</div>
+                      </div>
+                      <div id="state">已关注</div>
+                    </div>
+                </div>
+            </div>
+            <div id="body">
+                <h3>目录</h3>
+                <List
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={(item) => (
+      <List.Item>
+        <List.Item.Meta
+          title={<Link to='/article'>{item.title}</Link>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+        />
+      </List.Item>
+    )}
+  />
+            </div>
+        </div>
     )
 }
+
+
+
