2c9f4875b543ff86666ee7ed3294d059d0ea9628
‘ruijiaFor’ <3522443660@qq.com>
2022-09-05 20:09:48 +0800

    上传专栏图片

--- src/pages/Column/index.jsx
+++ src/pages/Column/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import 'antd/dist/antd.css';
-import { Descriptions, List } from 'antd';
+import { Descriptions, List,Upload,message } from 'antd';
 import { columnInfo } from '../../api/request';
 import { useSearchParams } from 'react-router-dom';
 import { follow } from '../../api/request';
 import { Link } from 'react-router-dom';
 import './index.css'
 export default function Column() {
+  
   const [search,setSearch] = useSearchParams() 
   const[newData,setNewData] = React.useState(null)
   const[isFollow,setIsFollow] = React.useState(true)
+  const id =search.get('id')
   React.useEffect(()=>{
-      const id =search.get('id')
       columnInfo(id).then((res)=>{
        const newData = res.data.data
        setNewData(newData)
@@ -25,14 +26,33 @@ export default function Column() {
 
   //  })
   }
-  console.log(isFollow);
+  const props = {
+    action: `api/weiboke/static/Image/cover?columnId=${id}`,
+    method:'post',
+    showUploadList:false,
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} 上传成功`);
+       setTimeout(() => {
+        window.location.reload()
+       }, 1000);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
   return newData ? (
     <div id='wrap'>
       <div id="top">
         <div id="top-left">
-          <div id="img-box">
-            <img src={newData.column.coverUrl} alt="" />
+        <Upload {...props}>
+      <div id="img-box">
+            <img src={newData.column.coverUrl} alt="支持png" />
           </div>
+  </Upload>
         </div>
         <div id="top-right">
           <Descriptions title={newData.column.title} style={{ fontSize: '20px' }}>
@@ -70,3 +90,5 @@ export default function Column() {
 
 
 
+
+ 
